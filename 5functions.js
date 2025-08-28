fdb803b7da5b4012f02bec8839bd698252482fae
aswinnn-lab <aswin.nair15@gmail.com>
2025-08-28 17:53:57 +0530

    Create 5functions.js

---
+++ 5functions.js
@@ -0,0 +1,17 @@
+function toggleDarkMode(){
+    const el=document.querySelector("body");
+    el.classList.add("darkMode");
+}
+
+function gridView(){
+    const el=document.querySelector("div");
+    el.classList.add("grid");
+}
+
+function colView(){
+    const el=document.querySelector("div");
+    el.classList.remove("grid");
+}
+
+
+
