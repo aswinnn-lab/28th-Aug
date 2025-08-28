e2ec2786a5a05d799af3642ebc56f4e28cf4686d
aswinnn-lab <aswin.nair15@gmail.com>
2025-08-28 15:02:13 +0530

    Update 2functions.js

---
+++ 2functions.js
@@ -0,0 +1,9 @@
+function gridView() {
+    const el=document.querySelector("div");
+    el.classList.add("grid");
+}
+
+function toggleDarkMode(){
+    const el=document.querySelector("body");
+    el.classList.toggle("darkMode")
+}
