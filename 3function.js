9d08956ba7f0162502d5e94f9834e3f4cbdd7c87
aswinnn-lab <aswin.nair15@gmail.com>
2025-08-28 15:32:30 +0530

    Create 3function.js

---
+++ 3function.js
@@ -0,0 +1,10 @@
+function gridView() {
+    const el=document.querySelector("div");
+    el.classList.add("grid");
+}
+
+function toggleDarkMode(){
+    const el=document.querySelector("body");
+    el.classList.toggle("darkMode")
+}
+
