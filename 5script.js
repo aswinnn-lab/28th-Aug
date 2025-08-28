a99e53ac2a5ee373767b0e6378649ab72aabe36e
aswinnn-lab <aswin.nair15@gmail.com>
2025-08-28 17:54:19 +0530

    Create 5script.js

---
+++ 5script.js
@@ -0,0 +1,9 @@
+function toggleMenu(){
+    const el=document.querySelector("header");
+    el.classList.toggle("hidden");
+}
+
+function galleryOn(){
+    const el=document.querySelector("div");
+    el.classList.add("border","bg-white");
+}
